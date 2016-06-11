14f9ae8a32b8ec8297c779f5c30569f76a9f8e23
Wolfgang Baird <aguywithlonghair@yahoo.com>
2016-06-11 12:45:48 -0700

    2.2.1
    - cache filters per window
    - add to filters rather than replace them all
    - general improvements
    - Grayifier > GrayFocus
    - Updated readme

---
+++ GrayFocus.m
@@ -0,0 +1,80 @@
+//
+//  GrayFocus.m
+//  GrayFocus
+//
+//  Created by Andy Matuschak on 8/5/10.
+//  Updated by Wolfgang Baird on 4/23/16.
+//
+//  Copyright 2010 Andy Matuschak. All rights reserved.
+//
+
+@import AppKit;
+#import <QuartzCore/QuartzCore.h>
+#import <objc/runtime.h>
+
+NSArray* GF_addFilter(NSArray* gray, NSArray* def)
+{
+    NSMutableArray *newFilters = [[NSMutableArray alloc] initWithArray:def];
+    [newFilters addObjectsFromArray:gray];
+    NSArray *result = [newFilters copy];
+    return result;
+}
+
+@interface Grayifier : NSObject
+@end
+
+@implementation Grayifier
+
+NSArray         *_grayFilters;
+static void     *filterCache = &filterCache;
+static void     *isActive = &isActive;
+
++ (void)load
+{
+    NSArray *blacklist = @[@"com.apple.notificationcenterui", @"com.google.chrome", @"com.google.chrome.canary"];
+    NSString *appID = [[NSBundle mainBundle] bundleIdentifier];
+    if (![blacklist containsObject:appID])
+    {
+        CIFilter *filt = [CIFilter filterWithName:@"CIColorMonochrome"];
+        [filt setDefaults];
+        [filt setValue:[CIColor colorWithRed:.3 green:.3 blue:.3 alpha:1] forKey:@"inputColor"];
+        
+        CIFilter *filt2 = [CIFilter filterWithName:@"CIGammaAdjust"];
+        [filt2 setDefaults];
+        [filt2 setValue:[NSNumber numberWithFloat:0.3] forKey:@"inputPower"];
+        
+        _grayFilters = [NSArray arrayWithObjects:filt, filt2, nil];
+        
+        NSNotificationCenter *center = [NSNotificationCenter defaultCenter];
+        [center addObserver:self selector:@selector(GF_grayWindow:) name:NSWindowDidResignKeyNotification object:nil];
+        [center addObserver:self selector:@selector(GF_grayWindow:) name:NSWindowDidResignMainNotification object:nil];
+        [center addObserver:self selector:@selector(GF_restoreColor:) name:NSWindowDidBecomeMainNotification object:nil];
+        [center addObserver:self selector:@selector(GF_restoreColor:) name:NSWindowDidBecomeKeyNotification object:nil];
+        
+        NSLog(@"GrayFocus loaded...");
+    }
+}
+
++ (void)GF_grayWindow:(NSNotification *)note
+{
+    NSWindow *win = note.object;
+    if (![objc_getAssociatedObject(win, isActive) boolValue]) {
+        NSArray *_defaultFilters = [[win.contentView superview] contentFilters];
+        objc_setAssociatedObject(win, filterCache, _defaultFilters, OBJC_ASSOCIATION_RETAIN);
+        [[win.contentView superview] setWantsLayer:YES];
+        [[win.contentView superview] setContentFilters:GF_addFilter(_grayFilters, _defaultFilters)];
+        objc_setAssociatedObject(win, isActive, [NSNumber numberWithBool:true], OBJC_ASSOCIATION_RETAIN);
+    }
+}
+
++ (void)GF_restoreColor:(NSNotification *)note
+{
+    NSWindow *win = note.object;
+    if ([objc_getAssociatedObject(win, isActive) boolValue]) {
+        [[win.contentView superview] setWantsLayer:YES];
+        [[win.contentView superview] setContentFilters:objc_getAssociatedObject(win, filterCache)];
+        objc_setAssociatedObject(win, isActive, [NSNumber numberWithBool:false], OBJC_ASSOCIATION_RETAIN);
+    }
+}
+
+@end
---
+++ README.md
@@ -0,0 +1,21 @@
+# GrayFocus
+
+![preview](preview.png)
+
+# Information:
+
+- Designed for 10.9+
+- SIMBL plugin to grayscale background windows on OS X
+- Author: [w0lfschild](https://github.com/w0lfschild)
+
+# Installation:
+
+1. Download [mySIMBL](https://github.com/w0lfschild/app_updates/raw/master/mySIMBL/mySIMBL_0.2.5.zip)
+2. Download [GrayFocus](https://github.com/w0lfschild/GrayFocus/raw/master/build/GrayFocus.zip)
+3. Unzip downloads
+4. Open `GrayFocus.bundle` with `mySIMBL.app`
+5. Restart Chrome
+
+### License:
+Pretty much the BSD license, just don't repackage it and call it your own please!    
+Also if you do make some changes, feel free to make a pull request and help make things more awesome!
